05a6cd5000f517b942f7b3216146a173b8e9ec8d
Kohei Kitamoto <k7_ou@kitamotokouhei-no-macbook-air.local>
2016-05-06 17:53:16 +0900

    Initial Commit

---
+++ SampleCustomClassDataStorage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCustomClassDataStorage
+//
+//  Created by Kohei Kitamoto on 2016/05/06.
+//  Copyright © 2016年 mocos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

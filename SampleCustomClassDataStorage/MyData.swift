b82fce2b0d2617aa2269f8f57fb201e0d6c88dcb
Kohei Kitamoto <k7_ou@kitamotokouhei-no-macbook-air.local>
2016-05-08 18:13:32 +0900

    Initial Commit

---
+++ SampleCustomClassDataStorage/MyData.swift
@@ -0,0 +1,13 @@
+//
+//  MyData.swift
+//  SampleCustomClassDataStorage
+//
+//  Created by Kohei Kitamoto on 2016/05/06.
+//  Copyright © 2016年 mocos. All rights reserved.
+//
+
+import Foundation
+
+class MyData {
+    var valueString :String?
+}
--- SampleCustomClassDataStorage/ViewController.swift
+++ SampleCustomClassDataStorage/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let data = MyData()
+        data.valueString = "test"
+        userDefaults.setObject(data, forKey: "data")
+        userDefaults.synchronize()
     }
 
     override func didReceiveMemoryWarning() {
